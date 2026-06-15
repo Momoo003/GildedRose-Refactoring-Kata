0610da2ee19606f1b0d989e069d2ff804a28345b
if25b221 <if25b221@technikum-wien.at>
2026-06-15 11:36:08 +0200

    Add tests for Sulfuras

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -61,6 +61,20 @@ class GildedRoseTest {
         assertEquals(50, items[0].quality);
     }
 
-    
+    // Add tests for Sulfuras
+
+    @Test
+    void sulfurasNeverChanges() {
+        Item[] items = {
+            new Item("Sulfuras, Hand of Ragnaros", 0, 80)
+        };
+
+        GildedRose app = new GildedRose(items);
+
+        app.updateQuality();
+
+        assertEquals(0, items[0].sellIn);
+        assertEquals(80, items[0].quality);
+    }
 
 }
